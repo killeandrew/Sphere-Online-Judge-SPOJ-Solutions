ba8611b5ce4ce82a66cd2eee9cfcfb12a5c9234a
kille <killeandrew@gmail.com>
2021-06-05 20:11:30 -0400

    update

---
+++ 1_Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,24 @@
+  
+/*
+    author: kille
+*/
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+ 
+using namespace std;
+ 
+ int main()
+ {
+     int a;
+     while(true)
+     {
+         cin >> a;
+         if (a == 42)
+            break;
+         else
+            cout << a << endl;
+     }
+     return 0;
+ }
